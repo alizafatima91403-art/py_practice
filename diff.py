95816ae109f77bff4557a849a2ffcf3383782b2b
alizafatima91403-art <alizafatima91403@gmail.com>
2025-09-24 05:34:28 -0400

    Add files via upload

---
+++ diff.py
@@ -0,0 +1,5 @@
+a = 20
+b = 16
+
+diff = a - b
+print(diff)
---
+++ input.py
@@ -0,0 +1,6 @@
+name = input("name: ")
+age = input("age: ")
+price = input("price: ")
+
+print("my name is", name , "and i am",age, "years old")
+
