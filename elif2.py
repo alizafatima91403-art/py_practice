da8369b664b43f50ddf97a76c17e940f178e48d6
alizafatima91403-art <alizafatima91403@gmail.com>
2025-10-18 08:56:01 -0400

    Add files via upload

---
+++ elif2.py
@@ -0,0 +1,5 @@
+age = int(input("enter: "))
+if age == 18:
+    print('elgble')
+else:
+    print('nt elgble')
---
+++ else.py
@@ -0,0 +1,10 @@
+age = int(input("enter ur age: "))
+
+if(age>18):
+    print("eligible for voting ")
+elif(age<=0):
+    print("invalid age ")
+else:
+    print("not eligible for vote ")
+
+
