40bc1c6dd54d44957435f188cca4b657bb5933ce
alizafatima91403-art <alizafatima91403@gmail.com>
2025-12-07 10:18:17 -0500

    Add files via upload

---
+++ str2function.py
@@ -0,0 +1,14 @@
+str = "aliza"
+print(str.replace("a", "b"))
+
+str = "aliza"
+print(str.capitalize)
+
+str = "aliza"
+print(str.endswith("za"))
+
+str = "aliza"
+print(str.find("a"))
+
+str = "aliza"
+print(str. count("a"))
